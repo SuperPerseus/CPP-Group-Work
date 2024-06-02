af6839f7ddfed04d90aaa0843f048c7d9a8fa3e1
safethewall <1569170396@qq.com>
2024-06-02 15:37:54 +0800

    用户文件完成

--- C++/errorcode.h
+++ C++/errorcode.h
@@ -1,6 +1,7 @@
 #pragma once
 /*
 	101:cant control the wallet file
+	102:invalid amount
 	103:cant save message
 	104:cant open the file
 	105:matchtime is different with seat record
--- C++/timestamp.h
+++ C++/timestamp.h
@@ -15,3 +15,18 @@ void timestamp() {
 		<< std::setw(2) << std::setfill('0') << st.wMinute << ":"
 		<< std::setw(2) << std::setfill('0') << st.wSecond << std::endl;
 }
+
+string returntimestamp() {
+    SYSTEMTIME st;
+    GetLocalTime(&st);
+
+    std::ostringstream oss;
+    oss << std::setw(4) << std::setfill('0') << st.wYear << "-"
+        << std::setw(2) << std::setfill('0') << st.wMonth << "-"
+        << std::setw(2) << std::setfill('0') << st.wDay << " "
+        << std::setw(2) << std::setfill('0') << st.wHour << ":"
+        << std::setw(2) << std::setfill('0') << st.wMinute << ":"
+        << std::setw(2) << std::setfill('0') << st.wSecond;
+
+    return oss.str();
+}
